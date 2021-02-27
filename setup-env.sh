2e19a1bdad5bddfdb4f812453c704022d3837e2f
Simon Elberich <simonelberich@gmail.com>
2021-02-27 18:17:51 +0100

    Updated setup script

--- setup-env.sh
+++ setup-env.sh
@@ -8,18 +8,9 @@ apt-get update -qq > /dev/null
 echo "[Info] Upgrading apt packages, this might take a while"
 apt-get upgrade -y -qq > /dev/null
 echo "[Info] Installing build requirements"
-apt-get install -y nasm -qq > /dev/null
-apt-get install -y xorriso -qq > /dev/null
-apt-get install -y grub-pc-bin -qq > /dev/null
-apt-get install -y grub-common -qq > /dev/null
-apt-get install -y build-essential -qq > /dev/null
+apt-get install -y nasm xorriso grub-pc-bin grub-common -qq > /dev/null
 echo "[Info] Installing gcc/binutils requirements"
-apt-get install -y bison -qq > /dev/null
-apt-get install -y flex -qq > /dev/null
-apt-get install -y libgmp3-dev -qq > /dev/null
-apt-get install -y libmpc-dev -qq > /dev/null
-apt-get install -y libmpfr-dev -qq > /dev/null
-apt-get install -y texinfo -qq > /dev/null
+apt-get install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo -qq > /dev/null
 
 echo "[Info] Downloading gcc ($GCC_VERSION) and binutils ($BINUTILS_VERSION)"
 mkdir -p env
