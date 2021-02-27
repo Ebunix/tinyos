a1e3f433aa95df8d36d335f9ba646010c3c470c8
Simon Elberich <simonelberich@gmail.com>
2021-02-27 19:20:35 +0100

    Update build environment

--- setup-env.sh
+++ setup-env.sh
@@ -1,20 +1,60 @@
 #!/bin/bash
 
-set GCC_VERSION = 10.2.0
-set BINUTILS_VERSION = 2.36.1
+GCC_VERSION=10.2.0
+BINUTILS_VERSION=2.36.1
 
 echo "[Info] Updating apt package cache"
-apt-get update -qq > /dev/null
+sudo apt-get update -qq > /dev/null
 echo "[Info] Upgrading apt packages, this might take a while"
-apt-get upgrade -y -qq > /dev/null
+sudo apt-get upgrade -y -qq > /dev/null
 echo "[Info] Installing build requirements"
-apt-get install -y nasm xorriso grub-pc-bin grub-common -qq > /dev/null
+sudo apt-get install -y nasm xorriso grub-pc-bin grub-common -qq > /dev/null
 echo "[Info] Installing gcc/binutils requirements"
-apt-get install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo -qq > /dev/null
+sudo apt-get install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo -qq > /dev/null
 
-echo "[Info] Downloading gcc ($GCC_VERSION) and binutils ($BINUTILS_VERSION)"
 mkdir -p env
 cd env
-wget https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz
-wget https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz
+
+if [ ! -d "gcc-$GCC_VERSION" ]; then
+    if [ ! -f "gcc-$GCC_VERSION.tar.xz" ]; then
+        echo "[Info] Downloading gcc ($GCC_VERSION)"
+        wget -q "https://ftp.gnu.org/gnu/gcc/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz"
+    fi
+    echo "[Info] Unpacking gcc"
+    tar -xf "gcc-$GCC_VERSION.tar.xz"
+fi
+
+if [ ! -d "binutils-$BINUTILS_VERSION" ]; then
+    if [ ! -f "binutils-$BINUTILS_VERSION.tar.xz" ]; then
+        echo "[Info] Downloading binutils ($BINUTILS_VERSION)"
+        wget -q "https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz" 
+    fi
+    echo "[Info] Unpacking binutils"
+    tar -xf "binutils-$BINUTILS_VERSION.tar.xz"
+fi
+
+export PREFIX="$HOME/opt/cross"
+export TARGET=x86_64-elf
+export PATH="$PATH:$PREFIX/bin"
+
+if [ ! -d "binutils-build" ]; then
+    mkdir -p binutils-build
+    cd binutils-build
+    ../binutils-$BINUTILS_VERSION/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+    make -j4
+    make install
+    cd ..
+fi
+if [ ! -d "gcc-build" ]; then
+    mkdir -p gcc-build
+    cd gcc-build
+    ../gcc-$GCC_VERSION/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+    make all-gcc -j4 
+    make all-target-libgcc -j4
+    make install-gcc
+    make install-target-libgcc 
+    cd ..
+fi
+
 cd ..
+echo "[Info] All done!"
