d920d80dc4aba0a3380a2d5e54ed1f1de37d7c36
Simon Elberich <simonelberich@gmail.com>
2021-02-27 19:25:17 +0100

    Add qemu-system-x86 to environment

--- setup-env.sh
+++ setup-env.sh
@@ -3,6 +3,6 @@ sudo apt-get update -qq > /dev/null
 echo "[Info] Upgrading apt packages, this might take a while"
 sudo apt-get upgrade -y -qq > /dev/null
 echo "[Info] Installing build requirements"
-sudo apt-get install -y nasm xorriso grub-pc-bin grub-common -qq > /dev/null
+sudo apt-get install -y nasm xorriso grub-pc-bin grub-common qemu-system-x86 -qq > /dev/null
 echo "[Info] Installing gcc/binutils requirements"
 sudo apt-get install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo -qq > /dev/null
