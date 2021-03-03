e6befcc99a59bf79314cb73655f400c9bd54d541
Simon Elberich <simonelberich@gmail.com>
2021-03-03 15:30:56 +0100

    Git username and mail setup

--- setup-env.sh
+++ setup-env.sh
@@ -8,3 +8,6 @@ echo "[Info] Installing build requirements"
 sudo apt-get install -y nasm xorriso grub-pc-bin grub-common qemu-system-x86 -qq > /dev/null
 echo "[Info] Installing gcc/binutils requirements"
 sudo apt-get install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo -qq > /dev/null
+
+git config --global user.email "" # Configure this
+git config --global user.name "" # Configure this
